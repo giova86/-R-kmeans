0e8209fd10ccdf2b8528b616c6472114bde033ea
giova86 <39263257+giova86@users.noreply.github.com>
2018-05-14 13:57:52 +0200

    Update km.R

--- km.R
+++ km.R
@@ -1,26 +1,40 @@
+# ============================================= #
 # cluster analysis
+# ============================================= #
 
 library("cluster") 
 library("factoextra") 
 library("magrittr")
 
+# ============================================= #
 # load dataset "USArrests"
+# ============================================= #
+
 data("USArrests")
 USArrests
 
+# ============================================= #
 # data preparation 
 # - remove na
 # - scale value
+# ============================================= #
+
 my_data <- USArrests %>%
     na.omit() %>%
     scale()
 str(my_data)
 
+# ============================================= #
 # show data
+# ============================================= #
+
 head(my_data, n = 8)
 head(my_data, n = 50)
 
+# ============================================= #
 # calculation of the distances with different methods
+# ============================================= #
+
 res.dist <- get_dist(USArrests, stand = TRUE, method = "euclidean")
 fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07")) +
     labs(title = "euclidean")
@@ -37,7 +51,10 @@ res.dist <- get_dist(USArrests, stand = TRUE, method = "minkowski")
 fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07")) +
     labs(title = "minkowski")
 
+# ============================================= #
 # to be fixed...
+# ============================================= #
+
 par(mfrow=c(2,2))
 fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07")) +
     labs(title = "euclidean")
@@ -49,14 +66,20 @@ fviz_dist(res.dist, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4
     labs(title = "minkowski")
 par(mfrow=c(1,1))
 
+# ============================================= #
 # Determining the optimal number of clusters
+# ============================================= #
+
 fviz_nbclust(my_data, FUNcluster = fanny, method = "gap_stat")
 fviz_nbclust(my_data, FUNcluster = kmeans, method = "gap_stat")
 fviz_nbclust(my_data, FUNcluster = hcut, method = "gap_stat")
 fviz_nbclust(my_data, kmeans, method = "silhouette")
 fviz_nbclust(my_data, kmeans, method = "wss")
 
+# ============================================= #
 # perform kmeans
+# ============================================= #
+
 set.seed(123)
 km.res <- kmeans(my_data, centers = 2, nstart = 25)
 
@@ -82,7 +105,10 @@ km.res$cluster
 canc <- data.frame(pam.res$clustering,km.res$cluster)
 
 
+# ============================================= #
 # hierarchical clustering
+# ============================================= #
+
 res.hc <- USArrests %>%
     scale() %>%
     dist(method = "euclidean") %>%
